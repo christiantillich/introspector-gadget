33979f6c83cf08eed52b17f93dc66b61b4f168a9
Christian Tillich <christiantillich@Christians-MacBook-Air.local>
2020-05-14 14:46:27 -0500

    I'm just guessing at this point.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/christiantillich/introspector_gadget",
-    packages=['introspector_gadget'],
+    #packages=['introspector_gadget'],
     py_modules=['introspector_gadget'],
     classifiers=[
         "Programming Language :: Python :: 3",
